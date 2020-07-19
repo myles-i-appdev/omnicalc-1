17372940902abc73133e90ea54baddcb5e5bf64a
Myles Iribarne <myles.iribarne@gmail.com>
2020-07-19 15:05:36 -0700

    square working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
         render({ :template =>"calculation_templates/square_form.html.erb"})
     end
 
+    def square_results
+        @input_number =  params.fetch("input_number").to_f
+        @result= (@input_number**2).to_s
+        render({ :template=>"calculation_templates/square_results.html.erb"})
+
+    end
+
 end
