f25b972538bad2049ba9b160afc94af994473184
Myles Iribarne <myles.iribarne@gmail.com>
2020-07-19 16:49:04 -0700

    finished hw assigment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+    # square
     def blank_square_form
         render({ :template =>"calculation_templates/square_form.html.erb"})
     end
@@ -7,7 +8,48 @@ class ApplicationController < ActionController::Base
         @input_number =  params.fetch("input_number").to_f
         @result= (@input_number**2).to_s
         render({ :template=>"calculation_templates/square_results.html.erb"})
-
     end
+
+
+    #square root
+    def blank_square_root_form
+        render({ :template =>"calculation_templates/square_root_form.html.erb"})
+    end
+
+    def square_root_results
+        @input_number =  params.fetch("input_number").to_f
+        @result= (@input_number**(0.5))
+        render({ :template=>"calculation_templates/square_root_results.html.erb"})
+    end
+
+    # random
+    def blank_random_form
+        render({ :template =>"calculation_templates/random_form.html.erb"})
+    end
+
+    def random_results
+        @min_number =  params.fetch("min_number").to_f
+        @max_number =  params.fetch("max_number").to_f
+        @result= rand(@min_number..@max_number)
+        render({ :template=>"calculation_templates/random_results.html.erb"})
+    end
+
+    # payment
+    def blank_payment_form
+        render({ :template =>"calculation_templates/payment_form.html.erb"})
+    end
+
+    def payment_results
+        @apr =  params.fetch("apr").to_f
+        @num_years =  params.fetch("num_years").to_f
+        @principal =  params.fetch("principal").to_f
+
+        i=@apr/12.0/100
+        months = @num_years*12.0
+
+        @monthly_payment=@principal*i / (1 - (1+i)**(-months))
+        render({ :template=>"calculation_templates/payment_results.html.erb"})
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
     get("/square/new",{ :controller => "application", :action => "blank_square_form"})
     get("/square/results",{ :controller => "application", :action => "square_results"})
 
+    get("/square_root/new",{ :controller => "application", :action => "blank_square_root_form"})
+    get("/square_root/results",{ :controller => "application", :action => "square_root_results"})
+
+    get("/payment/new",{ :controller => "application", :action => "blank_payment_form"})
+    get("/payment/results",{ :controller => "application", :action => "payment_results"})
+
+    get("/random/new",{ :controller => "application", :action => "blank_random_form"})
+    get("/random/results",{ :controller => "application", :action => "random_results"})
 
 end
